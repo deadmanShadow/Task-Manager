6e022e2a0c8ff1a6a015927ba708707e669eda96
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-01 14:29:13 +0600

    task modified

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,23 +1,37 @@
 import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { useDispatch } from 'react-redux';
+import { updateStatus } from '../../redux/features/tasks/taskSlice';
 
-const TaskCard = ({task}) => {
+const TaskCard = ({ task }) => {
+  const dispatch = useDispatch();
+
+  let updatedStatus;
+
+  if (task.status == 'pending') {
+    updatedStatus = 'running';
+  } else if (task.status == 'running') {
+    updatedStatus = 'done';
+  } else {
+    updatedStatus = 'archive'
+  }
+
   return (
-   <div className='bg-secondary/10 rounded-md p-5'>
-    <h1 className='text-lg font-semibold mb-3'>{task?.title}</h1>
-    <p className='mb-3'>{task?.description}</p>
-    <p className='text-sm'>Assigned to - {task?.assignedTo}</p>
-    <div className='flex justify-between mt-3'>
-      <p>{task?.date}</p>
-      <div className='flex gap-3'>
-        <button  title='Delete'>
-          <TrashIcon className='h-5 w-5 text-red-500'></TrashIcon>
-        </button>
-        <b title='In progress'>
-          <ArrowRightIcon className='h-5 w-5 text-primary'></ArrowRightIcon>
-        </b>
+    <div className='bg-secondary/10 rounded-md p-5'>
+      <h1 className='text-lg font-semibold mb-3'>{task?.title}</h1>
+      <p className='mb-3'>{task?.description}</p>
+      <p className='text-sm'>Assigned to - {task?.assignedTo}</p>
+      <div className='flex justify-between mt-3'>
+        <p>{task?.date}</p>
+        <div className='flex gap-3'>
+          <button title='Delete'>
+            <TrashIcon className='h-5 w-5 text-red-500'></TrashIcon>
+          </button>
+          <button onClick={() => dispatch(updateStatus({ id: task.id, status: updatedStatus }))} title='Update Status'>
+            <ArrowRightIcon className='h-5 w-5 text-primary'></ArrowRightIcon>
+          </button>
+        </div>
       </div>
     </div>
-   </div>
   );
 };
 
--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -7,12 +7,12 @@ const initialState = {
             status: 'pending',
             title: 'Remove Button',
             description:
-              'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+                'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
             date: '2023-08-28',
             assignedTo: 'Raihan Shamil',
             priority: 'high',
-          }
-        
+        }
+
     ],
 };
 const tasksSlice = createSlice({
@@ -22,16 +22,20 @@ const tasksSlice = createSlice({
         addTask: (state, { payload }) => {
 
             if (state.tasks.length == 0) {
-                state.tasks.push({id:1, status:"pending" , ...payload});
-            }else{
+                state.tasks.push({ id: 1, status: "pending", ...payload });
+            } else {
                 const lastElement = state.tasks.at(-1);
-                state.tasks.push({id:lastElement.id + 1,  status:"pending" , ...payload,});
+                state.tasks.push({ id: lastElement.id + 1, status: "pending", ...payload, });
             }
         },
-        removeTask: (state, payload) => {
+        removeTask: (state, { payload }) => {
             state.tasks = state.tasks.filter((item) => item.id !== payload);
-          },
+        },
+        updateStatus: (state, { payload }) => {
+            const target = state.tasks.find(item => item.id == payload.id);
+            target.status = payload.status;
+        }
     },
 });
-export const { addTask } = tasksSlice.actions;
+export const { addTask, updateStatus } = tasksSlice.actions;
 export default tasksSlice.reducer;
