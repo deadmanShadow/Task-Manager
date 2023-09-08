a93915c6620542b50dd55839c5fa121137c19feb
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-08 10:29:51 +0600

    task details added

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,16 +2,24 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { userTasks } from '../../redux/features/tasks/taskSlice';
+import { updateStatus, userTasks } from '../../redux/features/tasks/taskSlice';
+import TaskDetailsModal from './TaskDetailsModal';
 
 const MyTasks = () => {
   const { tasks, userSpecificTasks } = useSelector((state) => state.tasksSlice);
   const { name: userName } = useSelector((state) => state.userSlice);
-  console.log(userSpecificTasks);
+  const [isOpen, setIsOpen] = useState(false);
+  const [taskId,setTaskId] = useState(0);
 
   const dispatch = useDispatch();
+
+  const handleModal = (id) => {
+    setTaskId(id);
+    setIsOpen(!isOpen);
+  };
+
   useEffect(() => {
     dispatch(userTasks(userName))
   }, [userName, dispatch, tasks]);
@@ -19,6 +27,7 @@ const MyTasks = () => {
 
   return (
     <div>
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} id={taskId}></TaskDetailsModal>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userSpecificTasks?.map((item) => (<div
@@ -27,10 +36,10 @@ const MyTasks = () => {
         >
           <h1>{item.title}</h1>
           <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
+            <button onClick={()=> handleModal(item.id)} className="grid place-content-center" title="Details">
               <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
             </button>
-            <button className="grid place-content-center" title="Done">
+            <button onClick={() => dispatch(updateStatus({ id: item.id, status: 'done' }))} className="grid place-content-center" title="Done">
               <CheckIcon className="w-5 h-5 text-primary" />
             </button>
           </div>
